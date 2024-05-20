d52b7724c2d1ac955c12f070ea3e57612d911d6d
JaquesBaudelaire <168909535+JaquesBaudelaire@users.noreply.github.com>
2024-05-20 05:39:52 +0200

    Add files via upload

---
+++ oecd_countries.py
@@ -0,0 +1,99 @@
+# list with oecd countries
+oecd_countries = [
+    "Australia", "Austria", "Belgium", "Canada", "Chile", "Colombia",
+    "Costa Rica", "Czech Republic", "Denmark", "Estonia", "Finland",
+    "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland",
+    "Israel", "Italy", "Japan", "Korea", "Latvia", "Lithuania",
+    "Luxembourg", "Mexico", "Netherlands", "New Zealand", "Norway",
+    "Poland", "Portugal", "Slovak Republic", "Slovenia", "Spain",
+    "Sweden", "Switzerland", "Turkey", "United Kingdom", "United States"
+]
+
+# dictionary that assigns an image to each country
+
+oecd_images = {
+    "australia": "Images/Australia.png",
+    "austria": "Images/Europe/Austria.png",
+    "belgium": "Images/Europe/Belgium.png",
+    "canada": "Images/Canada.png",
+    "chile": "Images/Chile.png",
+    "colombia": "Images/Colombia.png",
+    "costa rica": "Images/Costa_Rica.png",
+    "czech republic": "Images/Europe/Czech_Republic.png",
+    "denmark": "Images/Europe/Denmark.png",
+    "estonia": "Images/Europe/Estonia.png",
+    "finland": "Images/Europe/Finland.png",
+    "france": "Images/Europe/France.png",
+    "germany": "Images/Europe/Germany.png",
+    "greece": "Images/Europe/Greece.png",
+    "hungary": "Images/Europe/Hungary.png",
+    "iceland": "Images/Europe/Iceland.png",
+    "ireland": "Images/Europe/Ireland.png",
+    "israel": "Images/Israel.png",
+    "italy": "Images/Europe/Italy.png",
+    "japan": "Images/Japan.png",
+    "korea": "Images/Korea.png",
+    "latvia": "Images/Europe/Latvia.png",
+    "lithuania": "Images/Europe/Lithuania.png",
+    "luxembourg": "Images/Europe/Luxembourg.png",
+    "mexico": "Images/Mexico.png",
+    "netherlands": "Images/Europe/Netherlands.png",
+    "new zealand": "Images/New_Zealand.png",
+    "norway": "Images/Europe/Norway.png",
+    "poland": "Images/Europe/Poland.png",
+    "portugal": "Images/Europe/Portugal.png",
+    "slovak republic": "Images/Europe/Slovakia.png",
+    "slovenia": "Images/Europe/Slovenia.png",
+    "spain": "Images/Europe/Spain.png",
+    "sweden": "Images/Europe/Sweden.png",
+    "switzerland": "Images/Europe/Switzerland.png",
+    "turkey": "Images/Europe/Turkey.png",
+    "united kingdom": "Images/Europe/United_Kingdom.png",
+    "united states": "Images/United_States.png"
+}
+
+# Dictionary of OECD countries and their ISO country codes
+oecd_iso_access = {
+    "Australia": "AUS",
+        "Belgium": "BEL",
+        "Chile": "CHL",
+        "Costa Rica": "CRI",
+        "Denmark": "DNK",
+        "Germany": "DEU",
+        "Estonia": "EST",
+        "Finland": "FIN",
+        "France": "FRA",
+        "Greece": "GRC",
+        "Ireland": "IRL",
+        "Iceland": "ISL",
+        "Israel": "ISR",
+        "Italy": "ITA",
+        "Japan": "JPN",
+        "Canada": "CAN",
+        "Colombia": "COL",
+        "Korea": "KOR",
+        "Latvia": "LVA",
+        "Lithuania": "LTU",
+        "Luxembourg": "LUX",
+        "Mexico": "MEX",
+        "New Zealand": "NZL",
+        "Netherlands": "NLD",
+        "Norway": "NOR",
+        "Austria": "AUT",
+        "Poland": "POL",
+        "Portugal": "PRT",
+        "Sweden": "SWE",
+        "Switzerland": "CHE",
+        "Slovak Republic": "SVK",
+        "Slovenia": "SVN",
+        "Spain": "ESP",
+        "Czech Republic": "CZE",
+    "Turkey": "TUR",
+    "Hungary": "HUN",
+    "United Kingdom": "GBR",
+    "United States": "USA"
+}
+
+
+# all graphs that compare multiple countries
+cross_country_graphs = ["Graph 2", "Graph 4", "Graph 5", "Graph 6", "Graph 7", "Graph 8"]
