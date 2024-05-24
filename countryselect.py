754bf2af41b5811124700130f18100d523fe6036
JaquesBaudelaire <168909535+JaquesBaudelaire@users.noreply.github.com>
2024-05-24 19:25:24 +0200

    Update countryselect.py

--- countryselect.py
+++ countryselect.py
@@ -1,24 +1,20 @@
 import customtkinter as ctk
 
-# preselected countries when displaying graphs comparing multiple countries
-preselected_countries = ["France", "Germany", "Japan", "Sweden", "Switzerland", "United Kingdom", "United States"]
-
 # all countries selected with checkmarks
-selected_countries = []
+selected_countries = ["Switzerland"]
 
 class CountrySelect(ctk.CTkToplevel):
-    def __init__(self, master=None, countries=[], preselected=[]):
+    def __init__(self, master=None, countries=[], selected_countries=[]):
         super().__init__(master)
         self.title("Select Country")
         self.countries = countries
-        self.preselected = preselected
         self.selected_countries = selected_countries
 
         # Create check buttons for each country
         self.check_buttons = []
         self.index_var = 0
         for i, country in enumerate(self.countries):
-            var_value = "on" if country in self.preselected else "off"
+            var_value = "on" if country in self.selected_countries else "off"
             self.var = ctk.StringVar(value=var_value)
             self.button = ctk.CTkCheckBox(self, text=country, variable=self.var, onvalue="on", offvalue="off")
             row = self.index_var // 3
@@ -35,11 +31,9 @@ class CountrySelect(ctk.CTkToplevel):
 
     def confirm(self):
         self.selected_countries.clear()
-        self.preselected.clear()
         for button in self.check_buttons:
             if button.cget("variable").get() == "on":
                 country_name = button.cget("text")
                 self.selected_countries.append(country_name)
-                self.preselected.append(country_name)
         print(self.selected_countries)
         self.destroy()
